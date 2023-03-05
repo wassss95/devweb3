61f5ca8a82973c4afd173dc30fbe0a544056b02d
Wassila El Farh <wassilaelfarh@macbook-air-de-wassila.home>
2023-03-05 17:20:09 +0100

    mAj

--- projetweb/src/app/__init__.py
+++ projetweb/src/app/__init__.py
@@ -8,7 +8,6 @@ import os
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 db = SQLAlchemy(app)
-#db.init_app(app)
 
 class Data(db.Model):
     __tabname = "data"
